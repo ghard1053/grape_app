102d651b2ba7ac36e59e8b385b61dac9341a5a79
Takao <>
2018-05-05 15:40:19 +0900

    first

---
+++ db/migrate/20180505060723_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string  :name
+      t.integer :age
+      t.text    :memo
+      t.timestamps
+    end
+  end
+end
